ae457cee42f5d869bb38eef7cc19c0d5a51ea84a
OmarP-hub <omy250@gmail.com>
2024-11-25 22:03:59 -0700

    Funcion invert-graph anadida

--- source/dag.rkt
+++ source/dag.rkt
@@ -3,14 +3,23 @@
 (require graph
          "ast.rkt")
 
-(define (has? list x)
-  (cond
-    [(null? list)
-     #f]
-    [(equal? (car list) x)
-     #t]
-    [else
-      (has? (cdr list) x)]))
+(define (invert-graph graph)
+  (define g-reverse (directed-graph null)) ;; Grafo dirigido invertido
+
+  ;; Función auxiliar para agregar los nodos inversos
+  (define (add-inverted-edges! current-node)
+    (define children (get-neighbors graph current-node))
+    ;; Si no tiene vecinos, asegúrate de agregarlo al grafo invertido
+    (if (null? children)
+        (add-directed-edge! g-reverse 'root current-node) ;; Si no tiene hijos, agrega a root
+        (for ([child children])
+          (add-directed-edge! g-reverse child current-node)
+          (add-inverted-edges! child))))
+  ;; Comienza desde cada nodo raíz (sin padres)
+  (let ([raiz (car (get-neighbors graph 'root))])
+    (add-inverted-edges! raiz))
+
+  g-reverse)
 
 (define (ast->dag formula)
   (define g
@@ -25,7 +34,7 @@
          (formula->node p graph current))]
       [(l-and exp1 exp2)
        (let [(current (gensym 'and))]
-         (add-directed-edge! graph prev-node current) 
+         (add-directed-edge! graph prev-node current)
          (formula->node exp1 graph current)
          (formula->node exp2 graph current))]
       [else
